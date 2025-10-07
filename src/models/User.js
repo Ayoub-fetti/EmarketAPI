4aa440ecc46980545707513239f4f1b5198bddd7
ayoubfetti <ayoubfetti.dev@gmail.com>
2025-10-07 14:23:09 +0100

    model User

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userShema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    }
+}, {
+    timestamps : true
+});
+
+module.exports = mongoose.model('User', userShema);
