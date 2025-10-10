f337ecef454bd2f3507f25512b9cbd00fbb364b7
ayoubfetti <ayoubfetti.dev@gmail.com>
2025-10-10 16:01:44 +0100

    validation yup

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,12 +2,13 @@ const Product = require('../models/Product');
 
 const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find({ isDeleted: { $ne: true } });
+        const products = await Product.find({ isDeleted: { $ne: true } }).populate('category');
         res.json(products);
     } catch (e) {
         res.status(500).json({error: e.message});
     }
 };
+
 const getProductDeleted = async (req, res) => {
     try {
         const products = await Product.find({ isDeleted: { $ne: false } });
@@ -31,10 +32,6 @@ const getProductById = async (req, res) => {
 
 const createProduct = async (req, res) => {
     try {
-        const {title, description, price, stock, category, imageUrl} = req.body;
-        if (!title || !description || !price || !stock || !category || !imageUrl) {
-            return res.status(400).json({ error: 'All fields are required'});
-        }
         const product = new Product(req.body);
         const savedProduct = await product.save();
         res.status(201).json(savedProduct);
@@ -57,8 +54,7 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
     try {
-        const product = await Product.findOneAndUpdate({ _id: req.params.id, isDeleted: { $ne: true } }, { isDeleted: true, deletedAt: new Date() }, { new: true }
-        );
+        const product = await Product.findOneAndUpdate({ _id: req.params.id, isDeleted: { $ne: true } }, { isDeleted: true, deletedAt: new Date() }, { new: true });
         if (!product) {
             return res.status(404).json({error: 'Product not found'});
         }
@@ -67,6 +63,7 @@ const deleteProduct = async (req, res) => {
         res.status(500).json({error: e.message});
     }
 };
+
 const searchProducts = async (req, res) => {
     try {
         const { title, category, minPrice, maxPrice, inStock } = req.query;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,5 @@
-const User = require ('../models/User');
+const User = require('../models/User');
 
-// recuperer tous les users
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.find().select('-password');
@@ -10,42 +9,37 @@ const getAllUsers = async (req, res) => {
     }
 };
 
-// recuperer un user avec id
-
 const getUserById = async (req, res) => {
     try {
         const user = await User.findById(req.params.id).select('-password');
         if (!user) {
             return res.status(404).json({error: 'User not found'});
         }
-    res.json(user);
+        res.json(user);
     } catch (e) {
         res.status(500).json({ error: e.message });
-        }
-    };
+    }
+};
+
 const createUser = async (req, res) => {
     try {
-        const {fullName, email, password, role} = req.body;
-        const existingUser = await User.findOne({email});
+        const existingUser = await User.findOne({email: req.body.email});
         if (existingUser) {
             return res.status(400).json({error: 'Email not available'});
         }
-        const user = new User({ fullName, email, password, role});
+        const user = new User(req.body);
         await user.save();
         const userResponse = user.toObject();
         delete userResponse.password;
-
         res.status(201).json(userResponse);
     } catch (e) {
         res.status(500).json({error: e.message});
     }
 };
 
-// todo : update user function
-
 const updateUser = async (req, res) => {
     try {
-        const user = await  User.findByIdAndUpdate({_id: req.params.id, isDeleted: {$ne: true}}, req.body, {new: true, runValidators: true});
+        const user = await User.findByIdAndUpdate({_id: req.params.id, isDeleted: {$ne: true}}, req.body, {new: true, runValidators: true});
         if (!user) {
             return res.status(404).json({error: 'User not found'});
         }
@@ -54,9 +48,7 @@ const updateUser = async (req, res) => {
         res.status(500).json({error: e.message});
     }
 };
-// todo : validation
 
-// delete user
 const deleteUser = async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
